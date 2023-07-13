511060c593dbf5b7a12b5a893073f916bcc750f8
minaripenguin <minaripenguin@users.noreply.github.com>
2023-07-14 07:56:18 +0800

    core: Workaround for ASI crash
    test: reboot with patch applied, check if ASI services crashes
    
    Change-Id: I104727c30ab34a69cc76955d084ef6741dafc13b
    Signed-off-by: aswin7469 <aswinas@pixysos.com>

--- core/java/com/android/internal/util/custom/PixelPropsUtils.java
+++ core/java/com/android/internal/util/custom/PixelPropsUtils.java
@@ -50,6 +50,8 @@ public class PixelPropsUtils {
     private static final ComponentName GMS_ADD_ACCOUNT_ACTIVITY = ComponentName.unflattenFromString(
             "com.google.android.gms/.auth.uiflows.minutemaid.MinuteMaidActivity");
 
+    private static final String PACKAGE_AIAI = "com.google.android.apps.miphone.aiai.AiaiApplication";
+
     private static final Map<String, Object> propsToChangeGeneric;
     private static final Map<String, Object> propsToChangePixel7Pro;
     private static final Map<String, Object> propsToChangePixel6Pro;
@@ -259,7 +261,7 @@ public class PixelPropsUtils {
 
     public static void setProps(String packageName) {
         propsToChangeGeneric.forEach((k, v) -> setPropValue(k, v));
-        if (packageName == null || packageName.isEmpty()) {
+        if (packageName == null || packageName.isEmpty() || packageName.equals(PACKAGE_AIAI)) {
             return;
         }
         if (Arrays.asList(packagesToKeep).contains(packageName)) {
